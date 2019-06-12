f523286aaac5d4e5d67446c8d8517d840257c652
AlejandroOrostica <alejandro.orostica@usach.cl>
2019-06-12 01:59:36 -0400

    estamos bien los 33 monitores

--- lab2.c
+++ lab2.c
@@ -18,14 +18,19 @@ typedef struct
     float ruido;
     float sumReales;
     float sumImaginarios;
-    int contador;   
+    float datosLeidos;
+    int datosEnBuffer;   
     pthread_mutex_t termina, lleno;
 
 }monitorHebra;
 
-typedef struct 
-{
-    pthread_cond_t enUso;
+typedef struct {
+    int hebra;
+    float mediaReal;
+    float mediaImaginaria;
+    float potencia;
+    float ruido;
+    pthread_mutex_t enUso;
 }monitorEscritura;
 
 monitorHebra* init_monitorHebra(int tamano){
@@ -37,9 +42,12 @@ monitorHebra* init_monitorHebra(int tamano){
     mH->ruido = 0.0;
     mH->sumImaginarios = 0.0;
     mH->sumReales = 0.0;
-    mH->contador = 0;
+    mH->datosLeidos = 0.0;
+    mH->datosEnBuffer = 0;
     pthread_mutex_init(&mH->termina,NULL);
     pthread_mutex_init(&mH->lleno,NULL);
+
+    return mH;
 
 }
 
@@ -71,13 +79,24 @@ float calcularDistancia(char** lista){
 }
 
 int main(int argc, char const *argv[]){
-    int i; 
+    int i, numeroDiscos, tamano, radio;
+    tamano = 10;
+    radio = 100;
     float distancia; 
     char buffer [100];
+    numeroDiscos = 2;
+    monitorHebra** arregloMonitores;
     char* linea= (char*)malloc(sizeof(char)*100);
     char** lista= (char**)malloc(sizeof(char*)*5);
     FILE* archivo;
-    printf("hola\n");
+
+    monitorHebra* mh = init_monitorHebra(2);
+    arregloMonitores = (monitorHebra**)malloc(sizeof(monitorHebra*)*2);
+    
+    for(i = 0; i<numeroDiscos ; i++){
+        arregloMonitores[i] = init_monitorHebra(tamano);
+    }
+
     archivo = fopen("prueba.csv","r");
     while(!feof(archivo)){
         int i=0;
@@ -94,10 +113,48 @@ int main(int argc, char const *argv[]){
     
         }
 
-        lista = procesarLinea(linea,lista);
+        lista = procesarLinea(linea,lista); // Lista = [Vu,Vv,V(u,v).r,V(u,v).i,V(u,v).w]
         distancia = calcularDistancia(lista);
-        printf("%s\n", lista[0]);
+        printf("la distancia es %f \n", distancia);
+        for(i = 0; i< numeroDiscos ; i++){
+            float min = radio * i;
+            float max= (radio*(i+1));
+            if(i==numeroDiscos-1 ){
+                
+                int j = 2; // J = 2 Para ignorar los dos primeros elementos de la lista
+                while(j<5){
+                    arregloMonitores[i]->buffer[arregloMonitores[i]->datosEnBuffer] = atof(lista[j]);
+                    j++;
+                    arregloMonitores[i]->datosEnBuffer++;
+                    
+
+                }
+                break;
+            }
+            else if(distancia<max && distancia>=min){
+                int j = 2 ;
+                while(j<5){
+                    arregloMonitores[i]->buffer[arregloMonitores[i]->datosEnBuffer] = atof(lista[j]);
+                    j++;
+                    arregloMonitores[i]->datosEnBuffer++;
+                    
+
+                }
+                break;
+            }
+
+
+        }
+        
+        
     }
+    for(i = 0; i <tamano*3 ; i++){
+        printf("%f\n",arregloMonitores[0]->buffer[i] );
+    }
+    for(i = 0; i <tamano*3 ; i++){
+        printf("%f\n",arregloMonitores[1]->buffer[i] );
+    }
+
     fclose(archivo);
 
     return 0;
